30c77f6145852cde8d255f3602bd58ac99032384
Annely Kurg <annely.kurg@khk.ee>
2021-12-08 20:34:01 +0200

    Delete from LS

--- classes/LS.js
+++ classes/LS.js
@@ -2,7 +2,7 @@ class LS {
     //help functions to get and send data from LS
     getData(name) {
         let data;
-        if(localStorage.getItem(name) === null){
+        if (localStorage.getItem(name) === null) {
             data = [];
         } else {
             data = JSON.parse(localStorage.getItem(name));
@@ -13,9 +13,21 @@ class LS {
     setData(name, data) {
         localStorage.setItem(name, JSON.stringify(data));
     }
-    addBook(book){
+
+    addBook(book) {
         let books = this.getData('books')
         books.push(book);
         this.setData('books', books);
+    }
+
+    delBook(book) {
+        let books = this.getData('books')
+        books.forEach(function (bookInLS, index) {
+            bookInLS = new Book(bookInLS.title, bookInLS.author, bookInLS.isbn)
+            if (bookInLS.title === book.title && bookInLS.author === book.author && bookInLS.isbn === book.isbn) {
+                book.splice(index, 1);
+            }
+        })
+        this.setData('books', book)
     }
 }
